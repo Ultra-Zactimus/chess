089f6675007611c93261f420d6a79b803bf79e57
Ultra Zacky <ultrazacky@gmail.com>
2022-02-21 15:58:12 -0800

    add change to numbers

--- program.cs
+++ program.cs
@@ -9,12 +9,12 @@ namespace Chessboard
   public static void Main()
   {
     Chess chess = new Chess();
-    Console.WriteLine("Please enter the x coordinate for the queen as a number: a(0), b(1), c(2), d(3), e(4), f(5), g(6), h(7)");
+    Console.WriteLine("Please enter the x coordinate for the queen as a number: a(1), b(2), c(3), d(4), e(5), f(6), g(7), h(8)");
     int queenX = int.Parse(Console.ReadLine());
     Console.WriteLine("Please enter the y coordinate for the queen: (1, 2, 3, 4, 5, 6, 7, 8)");
     int queenY = int.Parse(Console.ReadLine());
 
-    Console.WriteLine("Please enter the x coordinate for the opponent as a number: a(0), b(1), c(2), d(3), e(4), f(5), g(6), h(7)");
+    Console.WriteLine("Please enter the x coordinate for the opponent as a number: a(1), b(2), c(3), d(4), e(5), f(6), g(7), h(8)");
     int targetX = int.Parse(Console.ReadLine());
     Console.WriteLine("Please enter the y coordinate for the opponent: (1, 2, 3, 4, 5, 6, 7, 8)");
     int targetY = int.Parse(Console.ReadLine());
